6cba103aeb551d7049ba323e21867c17c79947da
donyang <donyangok@gmail.com>
2014-05-29 23:13:16 +0800

    fix IHeapCursor.java, Pair.java

--- main/src/info/guardianproject/database/IHeapCursor.java
+++ main/src/info/guardianproject/database/IHeapCursor.java
@@ -1,37 +1,33 @@
 package info.guardianproject.database;
 
+import info.guardianproject.database.sqlcipher.SQLiteCursorInJava$ICursorDataItemCreator;
 import android.database.Cursor;
 import android.util.SparseArray;
-import info.guardianproject.database.sqlcipher.SQLiteCursorInJava.ICursorDataItemCreator;
 
-public abstract interface IHeapCursor extends Cursor
+public interface IHeapCursor extends Cursor
 {
-  public abstract boolean checkIsCacheUseful(int paramInt);
-
-  public abstract boolean containKey(Object paramObject);
-
-  public abstract CursorDataItem getItem(int paramInt);
-
-  public abstract CursorDataItem getItemByKey(Object paramObject);
-
-  public abstract SparseArray[] getPosistionMaps();
-
-  public abstract boolean hasLoadAllData();
-
-  public abstract boolean isCacheUseful();
-
-  public abstract boolean notifyChange(Object paramObject, CursorDataItem paramCursorDataItem);
-
-  public abstract void putValue(int paramInt, Object paramObject);
-
-  public abstract void setAutoBuildData(boolean paramBoolean);
-
-  public abstract void setDataCreator(SQLiteCursorInJava.ICursorDataItemCreator paramICursorDataItemCreator);
-
-  public abstract void setPageSize(int paramInt);
+    boolean checkIsCacheUseful(int p0);
+    
+    boolean containKey(Object p0);
+    
+    CursorDataItem getItem(int p0);
+    
+    CursorDataItem getItemByKey(Object p0);
+    
+    @SuppressWarnings("rawtypes")
+	SparseArray[] getPosistionMaps();
+    
+    boolean hasLoadAllData();
+    
+    boolean isCacheUseful();
+    
+    boolean notifyChange(Object p0, CursorDataItem p1);
+    
+    void putValue(int p0, Object p1);
+    
+    void setAutoBuildData(boolean p0);
+    
+    void setDataCreator(SQLiteCursorInJava$ICursorDataItemCreator p0);
+    
+    void setPageSize(int p0);
 }
-
-/* Location:           /Users/don/Desktop/mm/com.tencent.mm/classes-dex2jar.jar
- * Qualified Name:     info.guardianproject.database.IHeapCursor
- * JD-Core Version:    0.6.2
- */
--- main/src/info/guardianproject/database/sqlcipher/Pair.java
+++ main/src/info/guardianproject/database/sqlcipher/Pair.java
@@ -2,48 +2,40 @@ package info.guardianproject.database.sqlcipher;
 
 public class Pair
 {
-  public final Object first;
-  public final Object second;
-
-  public Pair(Object paramObject1, Object paramObject2)
-  {
-    this.first = paramObject1;
-    this.second = paramObject2;
-  }
-
-  public static Pair create(Object paramObject1, Object paramObject2)
-  {
-    return new Pair(paramObject1, paramObject2);
-  }
-
-  public boolean equals(Object paramObject)
-  {
-    if (paramObject == this);
-    while (true)
-    {
-      return true;
-      if (!(paramObject instanceof Pair))
-        return false;
-      try
-      {
-        Pair localPair = (Pair)paramObject;
-        if ((!this.first.equals(localPair.first)) || (!this.second.equals(localPair.second)))
-          return false;
-      }
-      catch (ClassCastException localClassCastException)
-      {
-      }
+    public final Object first;
+    public final Object second;
+    
+    public Pair(final Object first, final Object second) {
+        super();
+        this.first = first;
+        this.second = second;
     }
-    return false;
-  }
-
-  public int hashCode()
-  {
-    return 31 * (527 + this.first.hashCode()) + this.second.hashCode();
-  }
+    
+    public static Pair create(final Object o, final Object o2) {
+        return new Pair(o, o2);
+    }
+    
+    @Override
+    public boolean equals(final Object o) {
+        if (o != this) {
+            if (!(o instanceof Pair)) {
+                return false;
+            }
+            try {
+                final Pair pair = (Pair)o;
+                if (!this.first.equals(pair.first) || !this.second.equals(pair.second)) {
+                    return false;
+                }
+            }
+            catch (ClassCastException ex) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 * (527 + this.first.hashCode()) + this.second.hashCode();
+    }
 }
-
-/* Location:           /Users/don/Desktop/mm/com.tencent.mm/classes-dex2jar.jar
- * Qualified Name:     info.guardianproject.database.sqlcipher.Pair
- * JD-Core Version:    0.6.2
- */
